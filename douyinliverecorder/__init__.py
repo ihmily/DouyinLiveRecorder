560f294819947d2d05f175651923d4d672043f05
ihmily <114978440+ihmily@users.noreply.github.com>
2024-10-10 13:53:21 +0800

    fix: update js path

--- douyinliverecorder/__init__.py
+++ douyinliverecorder/__init__.py
@@ -2,4 +2,4 @@ from pathlib import Path
 
 current_file_path = Path(__file__).resolve()
 current_dir = current_file_path.parent
-JS_SCRIPT_PATH = current_dir / 'js'
+JS_SCRIPT_PATH = current_dir / 'javascript'
